4cc2c780ee59a2057f12f985ba887b789c6d7c13
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2021-12-07 23:05:53 +0100

    feat(app): add candy

---
+++ exams/candy/init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "create lab.conf"
+touch lab.conf
+
+echo "create all startup"
+
+files=(as10r1 as20r1 as30r1 as40r1 as50r1 as60r1 as70r1 as80r1)
+
+for i in ${files[@]}; do
+    echo "ifconfig eth0 X.X.X.X/X up" >> $i.startup
+    echo "/etc/init.d/frr start" >> $i.startup
+done
+
+echo "create config"
+
+for i in ${files[@]}; do
+    mkdir -p $i/etc/frr
+    touch $i/etc/frr/daemons
+    touch $i/etc/frr/frr.conf
+    echo "service integrated-vtysh-config" >> $i/etc/frr/vtysh.conf
+    echo "hostname $i-frr" >> $i/etc/frr/vtysh.conf
+done
