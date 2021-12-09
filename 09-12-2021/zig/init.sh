a9f6ae3548ca7a94a778f7a3b9c8300294a1070f
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2021-12-09 22:40:51 +0100

    feat(app): add zig

---
+++ 09-12-2021/zig/init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "create lab.conf"
+touch lab.conf
+
+echo "create all startup"
+
+files=(as1 as2r1 as2r2 as3 as4 as5 as6 as7)
+
+for i in ${files[@]}; do
+    echo "ifconfig eth0 X.X.X.X/X up" >> $i.startup
+    echo "/etc/init.d/frr start" >> $i.startup
+done
+
+echo "create config"
+
+for i in ${files[@]}; do
+    mkdir -p $i/etc/frr
+    touch $i/etc/frr/daemons
+    touch $i/etc/frr/frr.conf
+    echo "service integrated-vtysh-config" >> $i/etc/frr/vtysh.conf
+    echo "hostname $i-frr" >> $i/etc/frr/vtysh.conf
+done
